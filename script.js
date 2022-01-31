4b10ce54b3776c1c821a4700ca6ba22850601d1e
UfukAldanmaz <61687476+UfukAldanmaz@users.noreply.github.com>
2022-01-31 15:05:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -12,11 +12,3 @@ document.getElementById("form").onsubmit = function () {
     return false;
 }
 
-//);
- //if (num < 0) return -1; else if (num === 0) return 1; else { return (num * factorialize(num - 1)) };
-// //
-//var num = window.prompt("Enter a number");
-//alert("The result is" + factorialize(num));
-/* add output for user   */
-// programı kullanıcıyla ilişkilendir// 
-//if (num < 0) return -1; else if (num === 0) return 1; else { return (num * factorialize(num - 1)) };//
